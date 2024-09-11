c2473defd54bec89520ec172ed84af5bffeb9a8e
Phatphum <ryw41541@rayongwit.ac.th>
2024-09-11 10:47:01 +0700

    view transitions api + techniques page

---
+++ components.js
@@ -0,0 +1,41 @@
+document.querySelector("navbar").innerHTML = `
+<nav>
+  <h1><a href="./index.html">เปียโน</a></h1>
+  <ul>
+    <li><a href="./index.html">หน้าหล้ก</a></li>
+    <li><a href="./register.html">สมัครสมาชิก</a></li>
+  </ul>
+</nav>
+`
+document.querySelector("page-footer").innerHTML = `
+<form class="comment">
+  <input placeholder="แสดงความคิดเห็น" />
+  <div class="send"><i class="material-icons">send</i></div>
+</form>
+
+
+<footer>
+  <h3>จัดทำโดย</h3>
+  <p><a href="./credits.html#p1">นายพัฒน์ภูมิ ทาสระคู</a></p>
+  <p><a href="./credits.html#p2">ด.ช.พศุตม์ เกศวพิทักษ์</a></p>
+  <p><a href="./credits.html#p3">นายกิณคาวรัก รานานารา</a></p>
+  <a href="./credits.html">ดูเพิ่มเติม</a>
+</footer>
+`
+
+{/* <div class="card-container">
+      <div class="card card1">
+        <div class="card-child">
+          <h3>ประวัติของเปียโน</h3>
+          <p>ศึกษาและเรียนรู้ที่มา ต้นกำเนิด และประวัติของเปียโน</p>
+          <button id="history">เรียนรู้เพิ่มเติม</button>
+        </div>
+      </div>
+      <div class="card card1">
+        <div class="card-child">
+          <h3>ประวัติของเปียโน</h3>
+          <p>ศึกษาและเรียนรู้ที่มา ต้นกำเนิด และประวัติของเปียโน</p>
+          <button id="history">เรียนรู้เพิ่มเติม</button>
+        </div>
+      </div>
+    </div> */}
